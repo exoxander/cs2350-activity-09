746f819af89d9a50cba1dc371b8a251bb25605a7
exoxander <exoxander15@gmail.com>
2021-11-13 20:04:07 -0700

    first commit

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,3 +6,9 @@ require.context('../stylesheets/', true, /\.(css)$/i)
 //TODO - Your ES6 JavaScript code (if any) goes here
 import 'bootstrap'
 
+import {movies} from "./movies";
+
+for(let m of movies){
+    let m_thumb = document.getElementById("m"+m.id);
+    m_thumb.innerHTML = `<img src="${m.poster}" alt="${m.title}" class="img-thumbnail"`;
+}
